00b5527ee77a9ab6a578020b5c575efee3dfbdf8
VictoriaArias-i <victoria.arias.me@gmail.com>
2025-06-18 09:23:03 -0400

    Update Clase_25.py

---
+++ Clase_25.py
@@ -0,0 +1,27 @@
+"""Clase 25     18-06-25"""
+
+"""Programar un menú de gestión de entradas para un concierto de GitHub con movimiento original, con su diferente show. el menú principal debe permitir 4 opciones.
+opciones:
+1) Venta de entradas.
+2) Cambio de show.
+3) Mostrar stock de funciones.
+4) salir del programa.
+Todas las opciones del menú deben estar implementadas mediante funciones.
+Al ingresar a la opción número 1:
+ -solicitar el nombre del usuario
+ -seleccionar el show 
+ *ambas por separado.
+ para que la solicitud sea exitosa, se deben cumplir los siguientes requerimientos:
+ -Nombre del comprador (no debe repetirse).
+ -la selección del show debe permitir seleccionar entradas para 1 de las dos funciones.
+  ~Movimiento original + tripulación.
+  ~Movimiento original + sonrisa.
+ -debe haber un máximo de 50 entradas para cada función.
+ -En caso de cumplir con todas los requerimientos, registrar entrada como exitosa.
+Al ingresar la opción número 2:
+ -El menú debe permitir cambiar el show seleccionado.
+Al ingresar la opción número 3:
+ -Mostrar el total de entradas disponibles para cada una de las funciones.
+Al ingresar la opción número 4:
+ -salir del programa
+En caso de elegir una opción valida, solicitar al usuario volver a ingresar una opción permitida."""
